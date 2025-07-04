500c9f4688f2a719a42af4ab12a33c94641c7506
rafaelgonnect <rafael@gonnect.com.br>
2025-07-04 13:38:49 -0300

    Update Swagger version to 1.2.0

--- config/swagger.js
+++ config/swagger.js
@@ -6,7 +6,7 @@ const swaggerOptions = {
     openapi: '3.0.0',
     info: {
       title: 'Gonnect CRM Webhook API',
-      version: '1.0.0',
+      version: '1.2.0',
       description: 'Sistema webhook para integração com Whaticket e gestão de CRM',
       contact: {
         name: 'Gonnect CRM',
